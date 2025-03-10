7390d44afa8ee78a35bd2983b7fd0ba0577b5e59
second2050 <git@second2050.me>
2025-03-11 00:58:29 +0100

    initial config

---
+++ configuration.nix
@@ -0,0 +1,215 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # custom packages
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        delugia-code = {
+          pname = "delugia-code";
+          version = "2404.23";
+          src = final.fetchzip {
+            url = "https://github.com/adam7/delugia-code/releases/download/v2404.23/delugia-complete.zip";
+            stripRoot = false;
+            hash = "sha256-0kl948agrzy300xl2ay0n4skm00i1axwd3n8s7qyzq44qm5j8nw7";
+          };
+          installPhase = ''
+            runHook preInstall
+            install -Dm644 delugia-complete/*.ttf -t $out/share/fonts/truetype
+            runHook postInstall
+          '';
+          meta = {
+            description = "customized cascadia-code";
+            homepage = "https://github.com/adam7/delugia-code";
+            license = lib.licenses.ofl;
+            maintainers = with lib.maintainers; [  ];
+          };
+        };
+      }
+    )
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.kernelParams = [
+    "apple_dcp.show_notch=1"
+  ];
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=2
+  '';
+
+  # hardware stuff
+  hardware = {
+    asahi = {
+      peripheralFirmwareDirectory = inputs.apple-silicon-firmware;
+      withRust = true;
+      useExperimentalGPUDriver = true;
+      # experimentalGPUInstallMode = "replace";
+    };
+    graphics.enable = true;
+  };
+
+  networking.hostName = "ringo";
+  networking.hostId = "a0fb3fd3";
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    # supportedLocales = [
+    #   "en_DK.UTF-8/UTF-8"
+    # ];
+    # extraLocaleSettings = {
+    #   LC_TIME = "en_DK.UTF-8";
+    # };
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        plasma6Support = true;
+        waylandFrontend = true;
+        addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-gtk
+        ];
+      };
+    };
+  };
+  console = {
+    # font = "Lat2-Terminus32";
+    keyMap = "uk";
+    useXkbConfig = false; # use xkb.options in tty.
+  };
+
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+
+  # Allow Unfree
+  nixpkgs.config.allowUnfree = true;
+  # nixpkgs.overlays = [ inputs.karuipkgs ];
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable KDE
+  services.xserver.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  services.desktopManager.plasma6.enable = true;
+  fonts = {
+    enableDefaultPackages = true;
+    packages = [
+      pkgs.cascadia-code
+    ];
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.karui = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      
+    ];
+    shell = pkgs.fish;
+  };
+
+  # Shell config
+  programs.fish.enable = true;
+  environment.shellAliases = lib.mkForce {}; # disable default shell aliases
+
+  # Applications
+  # programs.firefox.enable = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
+  };
+  programs.git.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Karui’s nixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-apple-silicon = {
+      url = "github:tpwrules/nixos-apple-silicon";
+    };
+    apple-silicon-firmware = {
+      url = "path:/boot/asahi";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    lix-module,
+    home-manager,
+    zen-browser,
+    nixos-apple-silicon,
+    apple-silicon-firmware }@inputs: {
+    nixosConfigurations.ringo = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      system = "aarch64-linux";
+      modules = [
+        (inputs.nixos-apple-silicon + /apple-silicon-support)
+        ./configuration.nix
+        # This is the important part -- add this line to your module list!
+        lix-module.nixosModules.default
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.karui = import ./home.nix;
+          home-manager.extraSpecialArgs = { inherit zen-browser; };
+        }
+      ];
+    };
+  };
+}
