05c901e9a267a7473f4ca2f8e2f65f3090b1b4cc
second2050 <git@second2050.me>
2025-03-16 17:38:23 +0100

    cleanup config for ringo

--- hosts/ringo/default.nix
+++ hosts/ringo/default.nix
@@ -10,15 +10,17 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = false;
-  boot.kernelParams = [
-    "apple_dcp.show_notch=1"
-  ];
-  boot.extraModprobeConfig = ''
-    options hid_apple fnmode=2
-  '';
+  # setup device bootloader and kernel cmdline
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = false;
+    kernelParams = [
+      "apple_dcp.show_notch=1"
+    ];
+    extraModprobeConfig = ''
+      options hid_apple fnmode=2
+    '';
+  };
 
   # hardware stuff
   hardware = {
@@ -32,89 +34,20 @@
   };
 
   # enable modules
-  karui.base.enable = true;
-  karui.desktop.enable = true;
+  karui = {
+    base.enable = true;
+    desktop.enable = true;
+  };
 
-  networking.hostName = "ringo";
-  networking.hostId = "a0fb3fd3";
+  # networking options
+  networking = {
+    hostName = "ringo";
+    hostId = "a0fb3fd3";
+  };
 
-  # Set your time zone.
-
-  # services.kmscon = {
-  #   enable = true;
-  #   extraConfig = ''
-  #     xkb-layout=gb
-  #   '';
-  #   fonts = [
-  #     {
-  #       name = "Delugia";
-  #       package = pkgs.delugia-code;
-  #     }
-  #   ];
-  # };
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Shell config
-
-  # Applications
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  # Initial NixOS Version, do *not* change.
+  # For more information, see `man configuration.nix`
+  # or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion
   system.stateVersion = "25.05"; # Did you read the comment?
 
 }
