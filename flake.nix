6d3953377c804da62df81cfa9e72e983031ed90b
second2050 <git@second2050.me>
2025-04-08 08:58:37 +0200

    update lock
    remove secondary home-manager input

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
     darkly-qt = {
       url = "github:Bali10050/Darkly";
