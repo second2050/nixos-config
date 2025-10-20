d52726479d0e8bfebb22509274ee35e353d8de63
second2050 <git@second2050.me>
2025-10-20 16:19:07 +0200

    include registry setup in standalone hm

--- homeModules/standalone/default.nix
+++ homeModules/standalone/default.nix
@@ -1,8 +1,25 @@
-{ pkgs, flakeDir, ... }:
+{
+  flakeDir,
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
 {
   home.packages = [ pkgs.fish ];
   programs.nh = {
     enable = true;
     homeFlake = flakeDir;
   };
+  nix.registry = lib.genAttrs (lib.filter (n: n != "self") (builtins.attrNames inputs)) (input: {
+    exact = true;
+    from = {
+      id = input;
+      type = "indirect";
+    };
+    to = {
+      path = inputs.${input}.outPath;
+      type = "path";
+    };
+  });
 }
