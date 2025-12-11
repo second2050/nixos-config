2270884873f1a8a6273c882c3ec1ff7e6d6c5069
Karui second2050 Hoshimiya <git@second2050.me>
2025-12-11 10:28:51 +0100

    fix typo

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -203,7 +203,7 @@ in
       enableUserSlices = true;
     };
 
-    # Firefox use CA store
+    # Zen use CA store
     environment.etc = {
       "zen/policies/policies.json".text = builtins.toJSON {
         policies.SecurityDevices.Add.p11-kit-trust = "${pkgs.p11-kit}/lib/pkcs11/p11-kit-trust.so";
