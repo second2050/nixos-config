992b92390a9a5acb3f28c2894da2d6969cbefdbc
Karui "second2050" Hoshimiya <git@second2050.me>
2026-02-26 15:18:10 +0100

    update fairydust kernel

--- pkgs/linux-asahi-fairydust/default.nix
+++ pkgs/linux-asahi-fairydust/default.nix
@@ -18,15 +18,15 @@ let
       inherit stdenv lib;
 
       pname = "linux-asahi-fairydust";
-      version = "6.18.7";
+      version = "6.18.10";
       modDirVersion = version;
       extraMeta.branch = "6.18";
 
       src = fetchFromGitHub {
         owner = "AsahiLinux";
         repo = "linux";
-        rev = "b0b5bbf9f67e9063af591e7a1187d37d0f2b8e29"; # branch = "fairydust"
-        hash = "sha256-CWKAyyGuu9jjcwN3Gz98ZjcTLRPQwnpKd32IyD4zsBw=";
+        rev = "61b6e714dd19b7bee1c0e6ec4234199e640c2932"; # branch = "fairydust"
+        hash = "sha256-5eAgJTKcRdjEFzHDSrh/XReaT6Db9YN2RN1SwOs28NE=";
       };
 
       kernelPatches = [
